68703842b8dd2e6a23b46b0f9d883e6ca2cda338
gabezeller <gabeaz03@gmail.com>
2025-01-04 23:18:23 -0600

    Added required images to files, began work on homepage componenet

---
+++ src/app/Components/homepage.jsx
@@ -0,0 +1,12 @@
+
+
+const HomePage = () => {
+    return (
+        
+        <div className="HomePage">
+            <img className="background" src="../images/redbackground.jpg"/>
+        </div>
+    )
+};
+
+export default HomePage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "./Components/header"
+import HomePage from "./Components/homepage"
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <HomePage/>
     </div>
   );
 }
