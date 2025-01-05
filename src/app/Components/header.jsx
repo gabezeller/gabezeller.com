ee3d4a23c028ad2342a7991b102fc0c02ff3651d
gabezeller <gabeaz03@gmail.com>
2025-01-04 22:39:59 -0600

    Added conditional formatting/styling based on screen size for nav bar

--- src/app/Components/header.jsx
+++ src/app/Components/header.jsx
@@ -1,7 +1,22 @@
+"use client";
+
+import { useState } from "react";
 import "./header.css";
 
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        if (menuOpen) {
+            document.body.style.overflow = "";
+            setMenuOpen(!menuOpen);
+        } else {
+            document.body.style.overflow = "hidden";
+            setMenuOpen(!menuOpen);
+        }
+        
+    }
 
     return (
         
@@ -9,7 +24,10 @@ const Header = () => {
             <div className="title">
                 <h1 className="title-text">Gabe Zeller</h1>
             </div>
-            <div className="navbar">
+            <button className='hamburger' onClick={toggleMenu}>
+                ☰
+            </button>
+            <div className={`navbar ${menuOpen ? "show" : ""}`}>
                 <div className="button projects-button" >
                     Projects
                 </div>
