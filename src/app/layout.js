5c2442c9b747182834775e824fff8d138d55b698
gabezeller <gabeaz03@gmail.com>
2025-08-20 00:55:23 -0500

    Added animated background to website, small design changes for project descriptions and about me page, added various animations

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import {  Handjet } from "next/font/google";
 import "./globals.css";
 import Header from "./Components/header"
+import { Fragment } from "react";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -24,15 +25,34 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+  
   return (
     <html lang="en" >
       <body
         className={handJet.className}
       >
-        <Header/>
-        <main>
-        {children}
-        </main>
+        <div className="backdrop">
+          <div className="circle-1"></div>
+          <div className="circle-2"></div>
+          <div className="circle-3"></div>
+          <div className="circle-4"></div>
+          <div className="circle-5"></div>
+          <div className="circle-6"></div>
+          <div className="circle-7"></div>
+          <div className="circle-8"></div>
+         
+          
+        </div>
+        
+          <Header/>
+          <main>
+          
+            {children}
+          
+          
+          </main>
+        
         
       </body>
     </html>
