0945328423e0f07a82799201ab33a2f54645366e
Gabe Zeller <gabeaz03@gmail.com>
2026-07-01 18:57:47 -0500

    Began planning of data migration in AWS s3 and postgresql database

---
+++ backend/scripts/dataMigration.js
@@ -0,0 +1,6 @@
+// use projects.json ot migrate data to the database
+
+
+// can use image file paths to upload images to S3 bucket and get the s3_key to store in the database
+
+// insert values into database
