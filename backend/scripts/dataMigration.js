b22a6c451e81ea3046dcf1dfadac626437ab6d08
Gabe Zeller <gabeaz03@gmail.com>
2026-07-02 20:08:08 -0500

    Data migration complete

--- backend/scripts/dataMigration.js
+++ backend/scripts/dataMigration.js
@@ -37,7 +37,7 @@ async function insertProject(project) {
 }
 
 async function insertUploadImages(project, project_id) {
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < project.images.length; i++) {
         const image = project.images[i];
         if (image.endsWith('.mp4')) {
             console.log(`Skipping video file: ${image}`);
@@ -45,23 +45,25 @@ async function insertUploadImages(project, project_id) {
         }
 
         const imagePath = `../../public${image}`;
-        console.log(`Processing image: ${imagePath}`);
         const imageKey = `project_image/${image.split('/').pop()}`;
+        console.log(`Processing image: ${imagePath}`);
         const uploadParams = {
             Bucket: 'amzn-s3-portfolio-images-603767527189-us-east-2-an',
             Key: imageKey,
-            Body: fs.createReadStream(imagePath),
-           
+            Body: imagePath.includes('embed') ? null : fs.createReadStream(imagePath),
+            
         };
 
         try {
-            if (!image.includes('embed')) {
+            console.log("imagePath.includes('embed'):", imagePath.includes('embed'));
+            if (!imagePath.includes('embed')) {
+                console.log(`Uploading image to S3: ${imagePath}`);
                 await s3Client.send(new PutObjectCommand(uploadParams));
-            }
+            } 
 
             await pool.query(
                 'INSERT INTO project_image (project_id, s3_key, alt_text, sort_order) VALUES ($1, $2, $3, $4)',
-                [project_id, imageKey, project.title, i]
+                [project_id, imagePath.includes('embed') ? image : imageKey, project.title, i]
             );
         } catch (error) {
             console.error(`Error processing image ${image}:`, error);
@@ -71,13 +73,12 @@ async function insertUploadImages(project, project_id) {
 }
 
 async function main() {
-    const project0 = data.projects[0];
-    const project_id = await insertProject(project0);
-    // await insertUploadImages(project0, project_id);
-    console.log(`Inserted project ${project0.title} with id ${project_id}`);
-
-    await insertUploadImages(project0, project_id);
-    console.log(`Inserted images for project ${project0.title}`);
+    for (const project of data.projects) {
+        const project_id = await insertProject(project);
+        console.log(`Inserted project ${project.title} with id ${project_id}`);
+        await insertUploadImages(project, project_id);
+        console.log(`Inserted images for project ${project.title}`);
+    }
     return;
 }
 
