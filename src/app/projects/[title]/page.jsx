793cd6412ac9a254ed94f48b442b938d37dd4eb1
Gabe Zeller <gabeaz03@gmail.com>
2026-07-06 17:10:52 -0500

    Integrated new api into project description page, minus some small things

--- src/app/projects/[title]/page.jsx
+++ src/app/projects/[title]/page.jsx
@@ -6,8 +6,10 @@ import 'react-slideshow-image/dist/styles.css';
 import Image from "next/image";
 import React from "react";
 import { useParams } from "next/navigation";
-import data from "../../projects.json";
+// import data from "../../projects.json";
 import Link from "next/link";
+import { getProjectBySlug } from "@/app/services/projectServices";
+import { useEffect } from "react";
 
 
 
@@ -17,10 +19,25 @@ import Link from "next/link";
 export default function Description () {
     // Get projectId from useParams
     const params = useParams();
-
+    console.log("params = ", params);
+    const [project, setProject] = React.useState(null);
     // Unpack project data
-    const {title, category, skills, bullets, paragraphs, images, embed} = data.projects[params.title];
+    // const {title, category, skills, bullets, paragraphs, images, embed} = data.projects[params.title];
+    const {title, technologies, content, images} = project ? project : {"title":"Title", "category":"Category", "skills":"Skills", "content":[],};
+    // fetch project/slug from backend API
+    useEffect(() => {
+        async function fetchProject(project_slug) {
+            try {
+                const response = await getProjectBySlug(project_slug);
+                setProject(response);
+                console.log("Fetched project data:", response);
+            } catch (error) {
+                console.error('Error fetching projects:', error);
+            }
+        }
 
+        fetchProject(params.title);
+    }, [params]);
     // console.log(images);
     // console.log(typeof(embed));
 
@@ -55,6 +72,7 @@ export default function Description () {
 
 
     return (
+        project ? (
     <div className={styles.description}>
         <Link href="/projects">
         <button className={styles.xButton} >x</button> 
@@ -63,11 +81,12 @@ export default function Description () {
             {title}
         </h2>
         <span className={styles.skills}>
-            {skills}
+            {technologies.map((tech, i) => <span key={i}>{tech}{i < technologies.length - 1 ? ", " : ""}</span>)}
+            {/* {skills} */}
         </span>
         <div className={styles.divider}></div>
         <ul className={styles.bullets}>
-            {bullets.map((bullet, index) => (<li className={styles.bullet} key={index}>{bullet}</li>))}
+            {content.bullets.map((bullet, index) => (<li className={styles.bullet} key={index}>{bullet}</li>))}
         </ul>
         <div className={styles.divider}></div>
 
@@ -85,21 +104,21 @@ export default function Description () {
                     
                     arrows={true} 
                     indicators={true}  >
-                    {images.slice(1).map((image, index) => (
+                    {images.slice(1).filter(image => !(image.url.includes("MP4")) && !(image.url.includes("mp4"))).map((image, index) => (
                         <div className={styles.imageSlide} key={index}>
-                            {image.includes("MP4") | image.includes("mp4") ? // render video if contains mp4, else image
+                            {image.url.includes("MP4") | image.url.includes("mp4") ? // render video if contains mp4, else image
                             (
                                 <video className={styles.projectImage} width="320" height="240" controls>
-                                    <source src={image} type="video/mp4" />
+                                    <source src={image.url} type="video/mp4" />
                                     Your browser does not support the video tag.
                               </video>
                             ) : 
                             (
-                                image.includes("embed") ? (
-                                    <iframe className={styles.embed} src={image} width="550" height="400"></iframe>
+                                image.url.includes("embed") ? (
+                                    <iframe className={styles.embed} src={image.url} width="550" height="400"></iframe>
                                 ) : 
                                 (
-                                <Image className={styles.projectImage} src={image} alt={title} width="550" height="400" />
+                                <Image className={styles.projectImage} src={image.url} alt={image.alt_text} width="550" height="400" />
                                 )
                             )}
                         </div>
@@ -116,11 +135,16 @@ export default function Description () {
             </div> 
             {/* )} */}
             <div className={styles.paragraphs}>
-                {paragraphs.map((paragraph, index) => (<p className={styles.paragraph} key={index}>{paragraph}</p>))}
+                {content.paragraphs.map((paragraph, index) => (<p className={styles.paragraph} key={index}>{paragraph}</p>))}
             </div>
         </div>
 
     </div>
+    ) : (
+        <div className={styles.loading}>
+            <p>Loading project...</p>
+        </div>
+    )
     ); 
 };
 
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -24,6 +24,7 @@ export default function Projects() {
         "images":[]
     };
 
+    // Fetch projects from backend API
     useEffect(() => {
         async function fetchProjects() {
             try {
